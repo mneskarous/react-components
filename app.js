6e8aed7138d33e0142a3ae061bc65add24c2463e
Michael Eskarous <mneskarous@gmail.com>
2019-07-29 13:26:03 -0700

    Implemented GroceryListItem class with function to make items bold with mouseOver

--- app.js
+++ app.js
@@ -2,18 +2,43 @@
 var App = () => (
   <div>
     <h2>My Grocery List</h2>
-    <GroceryList groceryItems = {['Spaghetti', 'Meatballs', 'Sauce']}/>
+    <GroceryList groceryItems={['Spaghetti', 'Meatballs', 'Sauce']}/>
   </div>
 );
 
 var GroceryList = (props) => (
   <ul>
-  {props.groceryItems.map(item => <li>{item}</li>) }
+    {props.groceryItems.map(item => 
+      <GroceryListItem item={item} />
+    )}
   </ul>
 );
 
-var GroceryListItem = (item) => (
-  // <li>{item}</li>
-);
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemMouseOver() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+    var style = {
+      fontWeight: this.state.done ? 'bold' : 'normal'
+    };
+    return (
+      <li style={style} onMouseOver={this.onListItemMouseOver.bind(this)} >{this.props.item}</li>
+    );
+  }
+
+}
+
  
 ReactDOM.render(<App />, document.getElementById("app"));
