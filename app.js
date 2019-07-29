79593bdc58f60f8d17960b4845d679786185d19b
Michael Eskarous <mneskarous@gmail.com>
2019-07-29 12:35:00 -0700

    Implemented App, GroceryList, and GroceryListItem components

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // TODO
+var App = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryList groceryItems = {['Spaghetti', 'Meatballs', 'Sauce']}/>
+  </div>
+);
+
+var GroceryList = (props) => (
+  <ul>
+  {props.groceryItems.map(item => <li>{item}</li>) }
+  </ul>
+);
+
+var GroceryListItem = (item) => (
+  // <li>{item}</li>
+);
+ 
+ReactDOM.render(<App />, document.getElementById("app"));
